d032ad403bb5ecac459123066d526baf0cd8ae9f
alcinacarlos <calcrom0607@g.educaand.es>
2023-11-09 19:19:17 +0100

    algoritmo_burbuja

--- src/algoritmo_burbuja.py
+++ src/algoritmo_burbuja.py
@@ -1,1 +1,15 @@
-a = [8, 3, 1, 19, 14]
+def algoritmo_burbuja(lista):
+    n = len(lista)
+    for i in range(n):
+        for x in range(0, n-i-1):
+            if lista[x] > lista[x+1]:
+                lista[x], lista[x+1] = lista[x+1], lista[x]
+    return lista
+
+def main():
+    a = [8, 3, 1, 19, 14]
+    print(algoritmo_burbuja(a))
+
+
+if __name__ == "__main__":
+    main()
